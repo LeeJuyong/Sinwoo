3d9b2ff8782a8d3d01a8edbc6a1e7958ac2c79cb
ggamgang <ggamgang@gmail.com>
2022-03-11 13:27:47 +0100

    tesetsetsetsetsetset

--- Sinwoo/src/SinwooInfo.java
+++ Sinwoo/src/SinwooInfo.java
@@ -3,7 +3,6 @@ public class SinwooInfo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome Sinwoo!!!!!!!!");
+		System.out.println("Welcome Sinwoo!!!!!!!!1");
 	}
-
 }
