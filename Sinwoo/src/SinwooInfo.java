9dc45eff46dd012591dab19cd4b27c112043999a
ggamgang <ggamgang@gmail.com>
2022-03-11 13:34:12 +0100

    tetetete

--- Sinwoo/src/SinwooInfo.java
+++ Sinwoo/src/SinwooInfo.java
@@ -3,6 +3,6 @@ public class SinwooInfo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome Sinwoo!!!!!!!!1");
+		System.out.println("Welcome Sinwoo!!!!!!!!2");
 	}
 }
